e5f7f71c9dbcf1eeb078af4855439c93288e256c
Lance Walker <lancewalker@Lances-MacBook-Pro.local>
2022-02-10 16:08:28 -0700

    use transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter/services.dart' show rootBundle;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
 
 void main() => runApp(const MyApp());
+
+final currencyFormatter = NumberFormat("#,##0.00", "en_US");
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -17,60 +20,10 @@ class MyApp extends StatelessWidget {
 
     return const MaterialApp(
       title: appTitle,
-      home: MyPokemon(title: appTitle),
+      home: MyTransactions(title: appTitle),
     );
   }
 }
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: FutureBuilder<List<Transaction>>(
-        future: fetchTransactions(http.Client()),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(
-              child: Text('An error has occurred!'),
-            );
-          } else if (snapshot.hasData) {
-             return TransactionList(transactions: snapshot.data!);
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-    );
-  }
-}
-
-// class TransactionList extends StatelessWidget {
-//   const TransactionList({Key? key, required this.transactions}) : super(key: key);
-//
-//   final List<Transaction> transactions;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return GridView.builder(
-//       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//         crossAxisCount: 2,
-//       ),
-//       itemCount: transactions.length,
-//       itemBuilder: (context, index) {
-//         return Image.network(transactions[index].thumbnailUrl);
-//       },
-//     );
-//   }
-// }
 
 class TransactionList extends StatelessWidget {
   const TransactionList({Key? key, required this.transactions}) : super(key: key);
@@ -88,10 +41,10 @@ class TransactionList extends StatelessWidget {
           itemBuilder: (BuildContext context,int index){
             return ListTile(
                 leading: Icon(Icons.list),
-                trailing: Text("GFG",
+                trailing: Text(transactions[index].description,
                   style: TextStyle(
                       color: Colors.green,fontSize: 15),),
-                title:Text("List item $index")
+                title:Text("\$${currencyFormatter.format(transactions[index].amount)}")
             );
           }
       ),
@@ -99,8 +52,8 @@ class TransactionList extends StatelessWidget {
   }
 }
 
-class MyPokemon extends StatelessWidget {
-  const MyPokemon({Key? key, required this.title}) : super(key: key);
+class MyTransactions extends StatelessWidget {
+  const MyTransactions({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -110,15 +63,15 @@ class MyPokemon extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: FutureBuilder<PokemonResponse>(
-        future: fetchPokemon(http.Client()),
+      body: FutureBuilder<List<Transaction>>(
+        future: fetchTransactions(http.Client()),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
             return const Center(
               child: Text('An error has occurred!'),
             );
           } else if (snapshot.hasData) {
-            return PokemonList(pokemonResponse: snapshot.data!);
+            return TransactionList(transactions: snapshot.data!);
           } else {
             return const Center(
               child: CircularProgressIndicator(),
@@ -129,35 +82,8 @@ class MyPokemon extends StatelessWidget {
     );
   }
 }
-
-class PokemonList extends StatelessWidget {
-  const PokemonList({Key? key, required this.pokemonResponse}) : super(key: key);
-
-  final PokemonResponse pokemonResponse;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title:Text("ListView.builder")
-      ),
-      body: ListView.builder(
-          itemCount: pokemonResponse.count,
-          itemBuilder: (BuildContext context,int index){
-            return ListTile(
-                leading: Text(pokemonResponse.results[index].name),
-                trailing: Text(pokemonResponse.results[index].url,
-                  style: const TextStyle(color: Colors.blue,fontSize: 15),),
-                title:Text("List item $index")
-            );
-          }
-      ),
-    );
-  }
-}
-
 Future<List<Transaction>> fetchTransactions(http.Client client) async {
-  final response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=1118'));
+  final response = await http.get(Uri.parse('https://houndstooth-mx.herokuapp.com/users/My-Unique-ID/transactions'));
 
   return compute(parseTransactions, response.body);
 }
@@ -172,7 +98,7 @@ class Transaction {
   final String date;
   final String description;
   final String type;
-  final int amount;
+  final double amount;
 
   const Transaction({
     required this.date,
@@ -189,43 +115,4 @@ class Transaction {
       amount: json['amount'],
     );
   }
-}
-
-Future<PokemonResponse> fetchPokemon(http.Client client) async {
-  final response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=1118'));
-
-  // Use the compute function to run parsePhotos in a separate isolate.
-  return pokemonFromJson(response.body);
-}
-
-PokemonResponse pokemonFromJson(String str) => PokemonResponse.fromJson(json.decode(str));
-
-class PokemonResponse {
-  int count;
-  List<Pokemon> results;
-
-  PokemonResponse({
-    required this.count,
-    required this.results
-  });
-
-  factory PokemonResponse.fromJson(Map<String, dynamic> json) => PokemonResponse(
-    count: json["count"],
-    results: List<Pokemon>.from(json["results"].map((x) => Pokemon.fromJson(x))),
-  );
-}
-
-class Pokemon {
-  String name;
-  String url;
-
-  Pokemon({
-    required this.name,
-    required this.url,
-  });
-
-  factory Pokemon.fromJson(Map<String, dynamic> json) => Pokemon(
-    name: json["name"],
-    url: json["url"],
-  );
 }
